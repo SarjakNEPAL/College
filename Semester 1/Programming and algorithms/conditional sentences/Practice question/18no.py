e34d771c2e4774c4755388dba5b5f7a8e2f16ca6
SarjakPC <bhandarisarjak@gmail.com>
2023-12-07 20:51:34 -0800

    Homework Complete

---
+++ Semester 1/Programming and algorithms/conditional sentences/Practice question/18no.py
@@ -0,0 +1,5 @@
+a=input("enter First Number")
+b=input("Enter Second number")
+c=input("Enter Mathematical operation")
+d=a+c+b
+print(eval(d))
---
+++ Semester 1/Programming and algorithms/conditional sentences/Practice question/qlast.py
@@ -0,0 +1,10 @@
+a=True
+b= True
+c=True
+d= True
+print(c)
+print(d)
+print(not a)
+print(not b)
+print(not c)
+print(not d)
