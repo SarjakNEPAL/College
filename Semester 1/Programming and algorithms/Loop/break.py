44eb5e0b5117b101bca78c67dd4a6b5d353c8de2
Sarjak404notfound <bhandarisarjak@gmail.com>
2023-12-15 12:12:42 +0545

    Before the practice question

---
+++ Semester 1/Programming and algorithms/Loop/break.py
@@ -0,0 +1,9 @@
+for i in range(1,5):
+    if i==3:
+        break # it terminates whole loop
+    print(i)
+
+for i in range(1,5):
+    if i==2:
+        continue # It ignores all the things after this and goes to next itiration
+    print(i)
