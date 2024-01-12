49ec2f1e7dfb8ae06c70bfb1fe54b7fb9c7f7e50
Sarjak404notfound <bhandarisarjak@gmail.com>
2024-01-12 13:11:19 +0545

    class

--- list tuple set dict/lab.py
+++ list tuple set dict/lab.py
@@ -4,3 +4,23 @@ a=(1,2,3,4,5)
 b=list(a)
 b.insert(2,"ram")
 print(tuple(b))
+
+
+
+
+a={"a":1}
+print(a.setdefault("a","momo"))
+print(a.setdefault("c","momo"))
+print(a)
+
+a["a"]:6
+print(a)
+
+
+oldkey='a'
+newkey='z'
+a[newkey]=a.pop(oldkey)
+print(a)
+
+for i in a.keys():
+    print(i)
